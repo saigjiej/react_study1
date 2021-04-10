9ccc20d871a3c44d98ae35b08a2fa9e653792745
SukMin Lee <s2019s16@e-mirim.hs.kr>
2021-04-10 23:49:58 +0900

    Controlled & Uncontrolled 컴포넌트

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import './jsx/create-react-element'
 // import './jsx/jsx-basic-1.js'
+
 // import './component-basic/component-test.js'
 // import './component-basic/props-basic-1'
 // import './component-basic/props-children'
@@ -9,5 +10,15 @@
 // import './component-intermediate/set-state-caveat'
 // import './component-intermediate/counter-demo.js'
 // import './component-intermediate/shallow-merge-caveat'
+
 // import './js-study/manipulate-list'
-import './component-intermediate/todo-app-demo'
+
+// import './component-intermediate/todo-app-demo'
+// import './component-intermediate/lifecycle-methods'
+// import './component-intermediate/timer-demo'
+// import './component-intermediate/lifecycle-method-should-component-update'
+// import './component-intermediate/force-update-demo'
+
+// import './component-advanced/ref-demo'
+// import './component-advanced/controlled-component'
+import './component-advanced/reservation-form'
